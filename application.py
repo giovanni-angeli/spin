a9dff41b5ab774d9e3f7534306bfedacd5764d6c
giovanni-angeli <giovanni.angeli6@gmail.com>
2015-01-22 17:26:07 +0100

    small corrections

--- application.py
+++ application.py
@@ -34,6 +34,33 @@ class Application(object):
                     ttl=30.,
                     ):
 
+        """ called to execute remote procedures.
+
+        arguments:
+
+        remote_id -- (str) id of the remote component to call the procedure on.
+
+        remote_callable -- (str) the method to be called on remote
+            component.
+
+        args -- (a jsonifyable list of objects) the args to be passed to
+            the remote method as arglist.
+
+        kwargs -- (a jsonifyable dict of objects) the args to be passed to
+            the remote method as keyword args.
+
+        answer_handler -- (a callable object) the callback, i.e. the function
+            to be called back when an answer to this call will be received. it
+            may be None
+
+        ttl -- time to live i.e. how long to wait for an answer to this call.
+            After ttl seconds since the call, if no answer has yet been
+            received, it is descarded emitting a warning.
+            Not used if answer_handler is None.
+
+        """
+
+
         protocol = self.remote_id2protocol[remote_id]
         protocol.remote_call(
             remote_callable,
